4d9b84fff6cdab2260bb3ddde23358b05a5f7d4f
Joshua Brunner <jbrunner@tccm.org>
2025-11-06 10:00:41 -0600

    more slashing

--- team-ts/src/App.tsx
+++ team-ts/src/App.tsx
@@ -3,7 +3,6 @@ import { StreamChat } from 'stream-chat';
 import { Chat } from 'stream-chat-react';
 
 import { getRandomImage } from './assets';
-import { useChecklist } from './ChecklistTasks';
 import { ChannelContainer } from './components/ChannelContainer/ChannelContainer';
 import { Sidebar } from './components/Sidebar/Sidebar';
 
@@ -13,13 +12,11 @@ const apiKey = urlParams.get('apikey') || process.env.REACT_APP_STREAM_KEY;
 const user = urlParams.get('user') || process.env.REACT_APP_USER_ID;
 const theme = urlParams.get('theme') || 'light';
 const userToken = urlParams.get('user_token') || process.env.REACT_APP_USER_TOKEN;
-const targetOrigin = urlParams.get('target_origin') || process.env.REACT_APP_TARGET_ORIGIN;
 
 const client = StreamChat.getInstance(apiKey!, { enableInsights: true, enableWSFallback: true });
 client.connectUser({ id: user!, name: 'Wheezie Test', image: getRandomImage() }, userToken);
 
 const App = () => {
-  useChecklist({ chatClient: client, targetOrigin: targetOrigin! });
 
   useEffect(() => {
     const handleColorChange = (color: string) => {
